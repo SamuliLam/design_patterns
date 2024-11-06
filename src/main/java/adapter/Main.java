e8a28db6a6ca2f155ae5b666eeb84507be8a4e89
Samuli Lamminmäki <36449245+SamuliLam@users.noreply.github.com>
2024-11-06 15:18:46 +0200

    Adapter done

---
+++ src/main/java/adapter/Main.java
@@ -0,0 +1,27 @@
+package adapter;
+
+import java.util.Calendar;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a Calendar instance
+        Calendar calendar = Calendar.getInstance();
+
+        // Create an instance of CalendarToNewDateAdapter
+        CalendarToNewDateAdapter adapter = new CalendarToNewDateAdapter(calendar);
+
+        // Set the date
+        adapter.setDay(15);
+        adapter.setMonth(Calendar.OCTOBER);
+        adapter.setYear(2023);
+
+        // Print the initial date
+        System.out.println("Initial date: " + adapter.getDay() + "/" + (adapter.getMonth() + 1) + "/" + adapter.getYear());
+
+        // Advance the date by several days
+        adapter.advanceDays(10);
+
+        // Print the new date
+        System.out.println("New date: " + adapter.getDay() + "/" + (adapter.getMonth() + 1) + "/" + adapter.getYear());
+    }
+}
